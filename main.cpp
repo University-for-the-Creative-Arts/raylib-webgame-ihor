9dcdfa9549210746a3204242e9dc853decdda541
isyvanenko <124314001+isyvanenko@users.noreply.github.com>
2025-10-02 12:35:00 +0100

    1

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 int main() {
     const int screenWidth = 960;
     const int screenHeight = 600;
+    Texture2D testSprite = LoadTexture("sprite.png");
 
     InitWindow(screenWidth, screenHeight, "Raylib Starter Template");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+            ClearBackground(RAYWHITE);
+            //Draw sprite at the position 
+            DrawTexture(testSprite, screenWidth / 2, screenHeight / 2, WHITE);
         EndDrawing();
+        
     }
-
+    UnloadTexture(testSprite);
     CloseWindow();
     return 0;
 }
